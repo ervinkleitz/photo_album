db0921e0d70693130c77995c77820c11d85aed84
Ervin Gonzales <ervinkleitz@gmail.com>
2015-03-23 23:55:31 -0700

    Added pictures and updated JS and index file

--- js/home.js
+++ js/home.js
@@ -1,12 +1,17 @@
 'use strict';
 
-        var ROOT_ALBUM_DIR = '/assets/album-covers';
+        var ROOT_ALBUM_DIR = 'assets/album-covers';
 
         // list all the img names that you want to be albums
         var albumImages = [
-            { path: "img001.png", title: "Cinque Terra", url: "/albums/cinque-terra" },
-            { path: "<your image name>", title: "<album title>", url: "<path>" },
-            { path: "<your image name>", title: "<album title>", url: "<path>" },
+            { path: "img001.png", title: "Pier 39", url: "assets/album-covers/img001.png" },
+            { path: "img002.png", title: "Chinatown", url: "assets/album-covers/img002.png" },
+            { path: "img003.png", title: "Lombard Street", url: "assets/album-covers/img003.png" },
+            { path: "img004.png", title: "Alcatraz", url: "assets/album-covers/img004.png" },
+            { path: "img005.png", title: "Cable Cars", url: "assets/album-covers/img005.png" },
+            { path: "img006.png", title: "Coit Tower", url: "assets/album-covers/img006.png" },
+            { path: "img007.png", title: "Twin Peaks", url: "assets/album-covers/img007.png" },
+            { path: "img008.png", title: "Golden Gate Park", url: "assets/album-covers/img008.png" }
             // copy/paste each `{ ... }` for as many albums as you have
             // Don't forget to have a comma after each `{ ... }` (except the last one!)
         ];
@@ -17,7 +22,7 @@
             // all of the album cover images below inside `$('')`
             // Remember: to select classes, start with a `.`
             // to select IDs, start with a `#`
-            var $albumRow = $(''); 
+            var $albumRow = $('#albumRow'); 
 
             // Create a new photo album object and give it the HTML
             // element that all albums cover `<img>` tags should be in
