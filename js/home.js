33de99339f46a1f13fffe14a8aca57ec7759fe45
Ervin Gonzales <ervinkleitz@gmail.com>
2015-03-25 11:06:53 -0700

    Added remaining 4 albums

--- js/home.js
+++ js/home.js
@@ -8,10 +8,10 @@
             { path: "img002.png", title: "Chinatown", url: "albums/chinatown/index.html" },
             { path: "img003.png", title: "Lombard Street", url: "albums/lombardstreet/index.html" },
             { path: "img004.png", title: "Alcatraz", url: "albums/alcatraz/index.html" },
-            { path: "img005.png", title: "Cable Cars", url: "index.html" },
-            { path: "img006.png", title: "Coit Tower", url: "index.html" },
-            { path: "img007.png", title: "Twin Peaks", url: "index.html" },
-            { path: "img008.png", title: "Golden Gate Park", url: "index.html" }
+            { path: "img005.png", title: "Cable Cars", url: "albums/cablecars/index.html" },
+            { path: "img006.png", title: "Coit Tower", url: "albums/coittower/index.html" },
+            { path: "img007.png", title: "Twin Peaks", url: "albums/twinpeaks/index.html" },
+            { path: "img008.png", title: "Golden Gate Park", url: "albums/goldengatepark/index.html" }
             // copy/paste each `{ ... }` for as many albums as you have
             // Don't forget to have a comma after each `{ ... }` (except the last one!)
         ];
